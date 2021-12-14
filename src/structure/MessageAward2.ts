6a32f408424e7c6d6fcd6f9c8d7da116d1621095
raziman <raziman.terra@gmail.com>
2021-12-14 15:03:11 +0800

    fix 24 hour issue

--- src/structure/MessageAward2.ts
+++ src/structure/MessageAward2.ts
@@ -11,17 +11,25 @@ export class MessageAward2 {
     if (!guild || msg.author.bot) return;
 
     const settings = Settings.fromGuild(guild);
+    const messageAward2 = settings.messageAward2;
 
-    if (msg.channel.id !== settings.messageAward2.channel.id) return;
+    if (msg.channel.id !== messageAward2.channel.id) return;
 
-    const startTime = DateTime.fromJSDate(settings.messageAward2.startTime).hour;
-    const endTime = DateTime.fromJSDate(settings.messageAward2.endTime).hour;
-    const now = DateTime.now().hour;
+
+    const now = DateTime.now();
+    const { day, month, year } = now;
+    const startTime = DateTime
+      .fromJSDate(messageAward2.startTime)
+      .set({ day, month, year });
+
+    const endTime = DateTime
+      .fromJSDate(messageAward2.endTime)
+      .set({ day, month, year });
 
     if (!(now >= startTime && now < endTime)) return;
 
     const member = msg.member;
-    const awardRole = settings.messageAward2.role;
+    const awardRole = messageAward2.role;
 
     if (member && !member.roles.cache.has(awardRole.id)) {
       member.roles.add(awardRole);
